0c0d93f336242468c4b6fcdc2003ac288786e91d
lancecrowder <thelancecrowder@gmail.com>
2018-07-25 14:42:58 -0500

    EOD - Push

--- dog.rb
+++ dog.rb
@@ -1,2 +1,16 @@
 class Dog
+
+  def talk(name)
+    puts "#{name} says \"Bark 'N Stuff!\""
+  end
+
+  def move(name, destination)
+    puts "#{name} is running to the #{destination}"
+  end
+
 end
+
+stripe = Dog.new
+
+stripe.talk("Stripe")
+stripe.move("Stripe","Hizzy fo Shizzy!")
