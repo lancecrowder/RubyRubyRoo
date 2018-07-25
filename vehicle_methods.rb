49e4ba656d7776c6ad6d7d5b475f9b2689eeda62
lancecrowder <thelancecrowder@gmail.com>
2018-07-25 13:46:35 -0500

    Vehicle started and default value for use_headlights() created

---
+++ vehicle_methods.rb
@@ -0,0 +1,18 @@
+def accelerate
+  puts "Stepping on the gas"
+  puts "Speeding up!!!"
+end
+
+def sound_horn
+  puts "Pressing the horn button"
+  puts "Beep Beep!!!"
+end
+
+def use_headlights (brightness = "Low-Beam")
+  puts "Turning on #{brightness} headlights"
+  puts "Watch out for Deer!!!"
+end
+
+sound_horn
+accelerate
+use_headlights()
