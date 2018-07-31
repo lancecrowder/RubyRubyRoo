50bebd55400bbdddc49b919e1164b079490517f8
lancecrowder <thelancecrowder@gmail.com>
2018-07-31 11:03:05 -0500

    Separated Employee from Salaried Employees so we can share employee attributes with Hourly Employees

--- employee.rb
+++ employee.rb
@@ -1,42 +1,19 @@
 class Employee
 
-  attr_accessor :name, :salary
+  attr_accessor :name
 
-  def name= (value)
-    if value == ""
+  def name=(name = "Anon E. Mouse")
+    if name == ""
       raise "Name cannot be Blank!!!"
     end
-    @name = value
+    @name = name
   end
 
-  def salary= (value)
-    if value <=0
-      raise "Salary value is not valid!"
-    end
-    @salary = value
+  def print_name
+    puts "Name: #{@name}"
   end
 
-  def initialize (name = "Anon E. Mouse", salary = 0.0)
-    self.name = name
-    self.salary = salary
-  end
-
-  def print_pay_stub
-    puts "Employee Name: #{@name}"
-    period_pay = (@salary/365.0) * 14
-    puts format("Pay This Period: $%.2f", period_pay)
-  end
-
-  kate = Employee.new("Kate Skate", 50000)
-  kate.print_pay_stub
-
-  puts "-" * 30
-
-  bob = Employee.new("Bob Oso", 55000)
-  bob.print_pay_stub
-
-  puts "-" * 30
-
-  employee = Employee.new("steve", -500)
-  employee.print_pay_stub
+  # bob = Employee.new
+  # bob.name = "Bob Oso"
+  # bob.print_name
 end
---
+++ salaried_employee.rb
@@ -0,0 +1,36 @@
+require "./employee"
+class SalariedEmployee < Employee
+  attr_accessor :salary
+
+  def salary= (salary)
+    if salary < 0
+      raise "Salary value is not valid!"
+    end
+    @salary = salary
+  end
+
+  # def initialize(name, salary = 0.0)
+  #   self.name = name
+  #   self.salary = salary
+  # end
+
+  def print_pay_stub
+    print_name
+    period_pay = (salary / 365.0) * 14
+    puts format("Pay This Period: $%.2f", period_pay)
+  end
+
+  kate = SalariedEmployee.new
+  kate.name = "Ema Nymton"
+  kate.salary = 50000
+  kate.print_pay_stub
+
+  puts "-" * 30
+
+  bob = SalariedEmployee.new
+  bob.name = "Bob Oso"
+  bob.salary = 55000
+  bob.print_pay_stub
+
+  puts "-" * 30
+end
