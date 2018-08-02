b237ddf78bb2f8ee232bcfe434f0198d83e755db
lancecrowder <thelancecrowder@gmail.com>
2018-08-02 10:02:55 -0500

    Steak sand chapter 10 started

---
+++ steak.rb
@@ -0,0 +1,18 @@
+class Steak
+  GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+
+  attr_accessor :grade
+
+  def >(other)
+    GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+  end
+end
+
+first_steak = Steak.new
+first_steak.grade = "Prime"
+second_steak = Steak.new
+second_steak.grade = "Choice"
+
+if first_steak > second_steak
+  puts "I'll take the #{first_steak.inspect}"
+end
