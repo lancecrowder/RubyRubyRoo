1a5633989ca309614be02b1577b926ec86030bfb
lancecrowder <thelancecrowder@gmail.com>
2018-08-03 09:53:55 -0500

    Test list with commas done.

--- TestStuff/lib/list_with_commas.rb
+++ TestStuff/lib/list_with_commas.rb
@@ -1,16 +1,21 @@
 class ListWithCommas
   attr_accessor :items
   def join
+    if items.length == 1
+      return items[0]
+    elsif items.length == 2
+      return "#{items[0]} and #{items[1]}"
+    end
     last_item = ", and #{items.last}"
     other_items = items.slice(0, items.length - 1).join(", ")
-    "#{other_items} #{last_item}"
+    "#{other_items}#{last_item}"
   end
 
-  two_subjects = ListWithCommas.new
-  two_subjects.items = ["my parents", "a rodeo clown"]
-  puts "A photo of #{two_subjects.join}"
+  # two_subjects = ListWithCommas.new
+  # two_subjects.items = ["my parents", "a rodeo clown"]
+  # puts "A photo of #{two_subjects.join}"
 
-  three_subject = ListWithCommas.new
-  three_subject.items = ["my parents", "a rodeo clown", "a prize bull"]
-  puts "A photo of #{three_subject.join}"
+  # three_subject = ListWithCommas.new
+  # three_subject.items = ["my parents", "a rodeo clown", "a prize bull"]
+  # puts "A photo of #{three_subject.join}"
 end
--- TestStuff/test/test_list_with_commas.rb
+++ TestStuff/test/test_list_with_commas.rb
@@ -1,2 +1,23 @@
-class TestListWithCommas
+require 'minitest/autorun'
+require 'list_with_commas'
+
+class TestListWithCommas < Minitest::Test
+  def test_it_joins_two_words_with_and
+    list = ListWithCommas.new
+    list.items = ["apple", "orange"]
+    assert("apple and orange" == list.join)
+  end
+
+  def test_it_joins_three_words_with_commas
+    list = ListWithCommas.new
+    list.items = ["apple", "orange", "pear"]
+    assert("apple, orange, and pear" == list.join)
+  end
+
+  def test_it_prints_one_word_alone
+    list = ListWithCommas.new
+    list.items = ["apple"]
+    # puts list.join
+    assert("apple" == list.join, "Lone 'Apple' failed!")
+  end
 end
