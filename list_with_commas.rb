514eb57c6b725c7ded0ac84e84cf2793e566b918
lancecrowder <thelancecrowder@gmail.com>
2018-08-03 08:22:44 -0500

    Chapter 13 and MiniTests started

--- list_with_commas.rb
+++ list_with_commas.rb
@@ -1,7 +1,7 @@
 class ListWithCommas
   attr_accessor :items
   def join
-    last_item = "and #{items.last}"
+    last_item = ", and #{items.last}"
     other_items = items.slice(0, items.length - 1).join(", ")
     "#{other_items} #{last_item}"
   end
