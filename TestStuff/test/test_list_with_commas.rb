780d7bfe91a54cebd81583a0d97ec763a5d1ed94
lancecrowder <thelancecrowder@gmail.com>
2018-08-03 10:14:46 -0500

    testing and chapter 13 complete!  On to a web app!! Chapter 14 began

--- TestStuff/test/test_list_with_commas.rb
+++ TestStuff/test/test_list_with_commas.rb
@@ -1,23 +1,23 @@
 require 'minitest/autorun'
 require 'list_with_commas'
 
-class TestListWithCommas < Minitest::Test
+class TestlistWithCommas < Minitest::Test
+  def setup
+    @list = ListWithCommas.new
+  end
+  
   def test_it_joins_two_words_with_and
-    list = ListWithCommas.new
-    list.items = ["apple", "orange"]
-    assert("apple and orange" == list.join)
+    @list.items = ["apple", "orange"]
+    assert("apple and orange" == @list.join)
   end
 
   def test_it_joins_three_words_with_commas
-    list = ListWithCommas.new
-    list.items = ["apple", "orange", "pear"]
-    assert("apple, orange, and pear" == list.join)
+    @list.items = ["apple", "orange", "pear"]
+    assert("apple, orange, and pear" == @list.join)
   end
 
   def test_it_prints_one_word_alone
-    list = ListWithCommas.new
-    list.items = ["apple"]
-    # puts list.join
-    assert("apple" == list.join, "Lone 'Apple' failed!")
+    @list.items = ["apple"]
+    assert("apple" == @list.join, "Lone 'Apple' failed!")
   end
 end
